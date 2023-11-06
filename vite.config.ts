aa73720a22bc7e686b8d561b368638ca2b511ff3
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-11-06 15:50:13 -0500

    .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import IstanbulPlugin from 'vite-plugin-istanbul'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [
